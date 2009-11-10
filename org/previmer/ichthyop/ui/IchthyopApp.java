4f7a8d9dc3780724a44df5ea1620271ed56a8525
pv1f08a <philippe.verley@ird.fr>
2009-11-10 17:23:26 +0000

    Created UI package and added the standard desktop application frames.

---
+++ org/previmer/ichthyop/ui/IchthyopApp.java
@@ -0,0 +1,44 @@
+/*
+ * IchthyopApp.java
+ */
+
+package org.previmer.ichthyop.ui;
+
+import org.jdesktop.application.Application;
+import org.jdesktop.application.SingleFrameApplication;
+
+/**
+ * The main class of the application.
+ */
+public class IchthyopApp extends SingleFrameApplication {
+
+    /**
+     * At startup create and show the main frame of the application.
+     */
+    @Override protected void startup() {
+        show(new IchthyopView(this));
+    }
+
+    /**
+     * This method is to initialize the specified window by injecting resources.
+     * Windows shown in our application come fully initialized from the GUI
+     * builder, so this additional configuration is not needed.
+     */
+    @Override protected void configureWindow(java.awt.Window root) {
+    }
+
+    /**
+     * A convenient static getter for the application instance.
+     * @return the instance of IchthyopApp
+     */
+    public static IchthyopApp getApplication() {
+        return Application.getInstance(IchthyopApp.class);
+    }
+
+    /**
+     * Main method launching the application.
+     */
+    public static void main(String[] args) {
+        launch(IchthyopApp.class, args);
+    }
+}
