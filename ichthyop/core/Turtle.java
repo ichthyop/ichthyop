7aae9b39334f0fc6dff6c3296650638ce6560538
pv1f08a <philippe.verley@ird.fr>
2010-02-01 15:13:54 +0000

    (Turtle) Just make sure Nathan receives the changes.

--- ichthyop/core/Turtle.java
+++ ichthyop/core/Turtle.java
@@ -1,5 +1,7 @@
 /*
- * To change this template, choose Tools | Templates
+ * February the 1st, 16:15. Nathan can you see that change ?
+ * 
+ To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
 package ichthyop.core;
