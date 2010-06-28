2c9879ac43253cd7a479405fddef625ff9c28d1c
pv1f08a <philippe.verley@ird.fr>
2010-06-28 14:57:13 +0000

    (ZoneEditor.xml) When creating a new zone file, open a template.

--- src/org/previmer/ichthyop/ui/ZoneEditor.java
+++ src/org/previmer/ichthyop/ui/ZoneEditor.java
@@ -42,6 +42,8 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import org.jdesktop.application.ResourceMap;
+import org.previmer.ichthyop.Template;
+import org.previmer.ichthyop.io.IOTools;
 import org.previmer.ichthyop.manager.SimulationManager;
 
 /**
@@ -165,6 +167,22 @@ public class ZoneEditor extends AbstractCellEditor implements ActionListener, Ta
             fileChooser.setSelectedFile(new File(path));
             int answer = fileChooser.showOpenDialog(panel);
             if (answer == JFileChooser.APPROVE_OPTION) {
+                File file = fileChooser.getSelectedFile();
+                if (!file.isFile()) {
+                    String msg = file.getAbsolutePath() + " does not exist. Create it ?";
+                    int create = JOptionPane.showConfirmDialog(panel, msg, "Create file ?", JOptionPane.YES_NO_CANCEL_OPTION);
+                    switch (create) {
+                        case JOptionPane.YES_OPTION:
+                            saveAndEditNewFile(file);
+                            break;
+                        case JOptionPane.NO_OPTION:
+                            actionPerformed(new ActionEvent(new JButton(), 0, EDIT));
+                            return;
+                        case JOptionPane.CANCEL_OPTION:
+                            fireEditingStopped();
+                            return;
+                    }
+                }
                 textField.setText(fileChooser.getSelectedFile().getAbsolutePath());
                 try {
                     zoneEditor.loadZonesFromFile(fileChooser.getSelectedFile());
@@ -190,7 +208,6 @@ public class ZoneEditor extends AbstractCellEditor implements ActionListener, Ta
                 File file = fileChooser.getSelectedFile();
                 if (file.exists()) {
                     String msg = file.getName() + " already exists. Overwrite it ?";
-
                     int overwrite = JOptionPane.showConfirmDialog(panel, msg, "Overwrite file ?", JOptionPane.YES_NO_CANCEL_OPTION);
                     switch (overwrite) {
                         case JOptionPane.YES_OPTION:
@@ -204,18 +221,27 @@ public class ZoneEditor extends AbstractCellEditor implements ActionListener, Ta
                             return;
                     }
                 }
-                System.out.println("file accepted " + file.getAbsolutePath());
-                textField.setText(file.getAbsolutePath());
-                try {
-                    zoneEditor.loadZonesFromFile(file);
-                    dialog.setVisible(true);
-                } catch (Exception ex) {
-                    SimulationManager.getLogger().log(Level.SEVERE, null, ex);
-                }
+                saveAndEditNewFile(file);
             } else {
                 fireEditingCanceled();
             }
         }
+    }
+
+    private void saveAndEditNewFile(File file) {
+        /* create the template */
+        try {
+            IOTools.copyFile(Template.getTemplate("zone.xml"), file);
+        } catch (Exception ex) {
+        }
+        /* edit the file */
+        textField.setText(file.getAbsolutePath());
+        try {
+            zoneEditor.loadZonesFromFile(file);
+        } catch (Exception ex) {
+            Logger.getLogger(ZoneEditor.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        dialog.setVisible(true);
     }
 
     public Object getCellEditorValue() {
