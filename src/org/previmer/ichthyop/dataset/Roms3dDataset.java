44767a4b2e5ee9c229123aaf5963a479677bcce3
pv1f08a <philippe.verley@ird.fr>
2010-06-30 15:46:30 +0000

    (Roms3dDataset.java) Get Cs_r and Cs_w same way than HC : first check as global attributes, then as variables and throw exception if not found. Added (but commented for now) the NewVertCoord calculation of the unperturbated depth, according https://www.myroms.org/forum/viewtopic.php?p=1664#p1664

--- src/org/previmer/ichthyop/dataset/Roms3dDataset.java
+++ src/org/previmer/ichthyop/dataset/Roms3dDataset.java
@@ -84,16 +84,63 @@ public class Roms3dDataset extends RomsCommon {
      */
     String strCs_r, strCs_w, strSc_r, strSc_w, strPn, strPm, strHC;
 
-    float getHc() throws IOException {
+    double getHc() throws IOException {
 
         if (null != ncIn.findGlobalAttribute(strHC)) {
-            /* UCLA */
+            /* supposedly UCLA */
             return ncIn.findGlobalAttribute(strHC).getNumericValue().floatValue();
         } else if (null != ncIn.findVariable(strHC)) {
-            /* Rutgers */
+            /* supposedly Rutgers */
             return ncIn.findVariable(strHC).readScalarFloat();
         } else {
+            /* hc not found */
             throw new IOException("S-coordinate critical depth (hc) could not be found, neither among variables nor global attributes");
+        }
+    }
+
+    double[] getCs_r() throws IOException {
+        if (null != ncIn.findGlobalAttribute(strCs_r)) {
+            /* supposedly UCLA */
+            Attribute attrib_cs_r = ncIn.findGlobalAttribute(strCs_r);
+            double[] Cs_r = new double[attrib_cs_r.getLength()];
+            for (int k = 0; k < Cs_r.length - 1; k++) {
+                Cs_r[k] = attrib_cs_r.getNumericValue(k).floatValue();
+            }
+            return Cs_r;
+        } else if (null != ncIn.findVariable(strCs_r)) {
+            /* supposedly Rutgers */
+            Array arr_cs_r = ncIn.findVariable(strCs_r).read();
+            double[] Cs_r = new double[arr_cs_r.getShape()[0]];
+            for (int k = 0; k < Cs_r.length - 1; k++) {
+                Cs_r[k] = arr_cs_r.getFloat(k);
+            }
+            return Cs_r;
+        } else {
+            /* Cs_w not found */
+            throw new IOException("S-coordinate stretching curves at Rho-points (Cs_r) could not be found, neither among variables nor global attributes");
+        }
+    }
+
+    double[] getCs_w() throws IOException {
+        if (null != ncIn.findGlobalAttribute(strCs_w)) {
+            /* supposedly UCLA */
+            Attribute attrib_cs_w = ncIn.findGlobalAttribute(strCs_w);
+            double[] Cs_w = new double[attrib_cs_w.getLength()];
+            for (int k = 0; k < Cs_w.length - 1; k++) {
+                Cs_w[k] = attrib_cs_w.getNumericValue(k).floatValue();
+            }
+            return Cs_w;
+        } else if (null != ncIn.findVariable(strCs_w)) {
+            /* supposedly Rutgers */
+            Array arr_cs_w = ncIn.findVariable(strCs_w).read();
+            double[] Cs_w = new double[arr_cs_w.getShape()[0]];
+            for (int k = 0; k < Cs_w.length - 1; k++) {
+                Cs_w[k] = arr_cs_w.getFloat(k);
+            }
+            return Cs_w;
+        } else {
+            /* Cs_w not found */
+            throw new IOException("S-coordinate stretching curves at W-points (Cs_w) could not be found, neither among variables nor global attributes");
         }
     }
 
@@ -155,44 +202,25 @@ public class Roms3dDataset extends RomsCommon {
     void getCstSigLevels() throws IOException {
 
         double hc;
-        double sc_r;
+        double[] sc_r = new double[nz];
         double[] Cs_r = new double[nz];
-        double[] cff_r = new double[nz];
-        double sc_w;
+        double[] sc_w = new double[nz + 1];
         double[] Cs_w = new double[nz + 1];
-        double[] cff_w = new double[nz + 1];
 
         //-----------------------------------------------------------
-        // Read the Param in ncIn
+        // Read hc, Cs_r and Cs_w in the NetCDF file.
         hc = getHc();
-        Attribute attrib_sc_w = ncIn.findGlobalAttribute(strSc_w);
-        Attribute attrib_sc_r = ncIn.findGlobalAttribute(strSc_r);
-        Attribute attrib_cs_w = ncIn.findGlobalAttribute(strCs_w);
-        Attribute attrib_cs_r = ncIn.findGlobalAttribute(strCs_r);
+        Cs_r = getCs_r();
+        Cs_w = getCs_w();
 
 
         //-----------------------------------------------------------
-        // Calculation of the Coeff;
+        // Calculation of sc_r and sc_w, the sigma levels
         for (int k = nz; k-- > 0;) {
-            if (null != attrib_sc_r) {
-                sc_r = attrib_sc_r.getNumericValue(k).floatValue();
-            } else {
-                sc_r = ((double) (k - nz) + .5d) / (double) nz;
-            }
-            Cs_r[k] = attrib_cs_r.getNumericValue(k).floatValue();
-            cff_r[k] = hc * (sc_r - Cs_r[k]);
+            sc_r[k] = ((double) (k - nz) + .5d) / (double) nz;
+            sc_w[k + 1] = (double) (k + 1 - nz) / (double) nz;
         }
-
-        for (int k = nz + 1; k-- > 0;) {
-            if (null != attrib_sc_w) {
-                sc_w = attrib_sc_w.getNumericValue(k).floatValue();
-            } else {
-                sc_w = (double) (k - nz) / (double) nz;
-            }
-            Cs_w[k] = attrib_cs_w.getNumericValue(k).floatValue();
-            cff_w[k] = hc * (sc_w - Cs_w[k]);
-        }
-        Cs_w[0] = -1.d;
+        sc_w[0] = -1.d;
 
         //------------------------------------------------------------
         // Calculation of z_w , z_r
@@ -203,25 +231,29 @@ public class Roms3dDataset extends RomsCommon {
             for (int j = ny; j-- > 0;) {
                 z_w_tmp[0][j][i] = -hRho[j][i];
                 for (int k = nz; k-- > 0;) {
-                    z_r_tmp[k][j][i] = cff_r[k] + Cs_r[k] * hRho[j][i];
-                    z_w_tmp[k
-                            + 1][j][i] = cff_w[k + 1] + Cs_w[k + 1] * hRho[j][i];
-
+                    z_r_tmp[k][j][i] = hc * (sc_r[k] - Cs_r[k]) + Cs_r[k] * hRho[j][i];
+                    z_w_tmp[k + 1][j][i] = hc * (sc_w[k + 1] - Cs_w[k + 1]) + Cs_w[k + 1] * hRho[j][i];
                 }
                 z_w_tmp[nz][j][i] = 0.d;
             }
         }
-        //z_rho_cst = new double[nz][ny][nx];
-        //z_w_cst = new double[nz + 1][ny][nx];
+        /* :VertCoordType = "NEW" see https://www.myroms.org/forum/viewtopic.php?p=1664#p1664
+         for (int i = nx; i-- > 0;) {
+            for (int j = ny; j-- > 0;) {
+                z_w_tmp[0][j][i] = -hRho[j][i];
+                for (int k = nz; k-- > 0;) {
+                    z_r_tmp[k][j][i] = hRho[j][i] * (sc_r[k] * hc + Cs_r[k] * hRho[j][i]) / (hc + hRho[j][i]);
+                    z_w_tmp[k + 1][j][i] = hRho[j][i] * (sc_w[k + 1] * hc + Cs_w[k + 1] * hRho[j][i]) / (hc + hRho[j][i]);
+                }
+                z_w_tmp[nz][j][i] = 0.d;
+            }
+        }*/
 
         z_rho_cst = z_r_tmp;
         z_w_cst = z_w_tmp;
 
         z_w_tp0 = new double[nz + 1][ny][nx];
         z_w_tp1 = new double[nz + 1][ny][nx];
-
-        //System.out.println("cst sig ok");
-
     }
 
     @Override
