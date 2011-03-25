55dfd1158bed67079b8edfc2f03476f4e12ee6a7
cl1ed68 <christophe.lett@ird.fr>
2011-03-25 10:20:32 +0000

    Recruitment Stain Added a new action to be able to recruit particles within a geographic area defined the same way as it is in Release Stain : center od the recruitment stain, a radius, a depth and a thickness. Keept the same recruitment criterions, based on age or size. Created a new recruitment_stain tracker :int  recruited_stain(time, drifter) = [0, 1] Only updated template Mars3D, still have to do the other ones.

---
+++ src/org/previmer/ichthyop/io/RecruitmentStainTracker.java
@@ -0,0 +1,69 @@
+/*
+ *  Copyright (C) 2011 mcuif
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.previmer.ichthyop.io;
+
+import java.util.Iterator;
+import org.previmer.ichthyop.TypeZone;
+import org.previmer.ichthyop.Zone;
+import org.previmer.ichthyop.arch.IBasicParticle;
+import org.previmer.ichthyop.arch.IRecruitableParticle;
+import org.previmer.ichthyop.particle.RecruitableParticleLayer;
+import ucar.ma2.Array;
+import ucar.ma2.ArrayBoolean;
+import ucar.ma2.ArrayInt;
+import ucar.ma2.DataType;
+import ucar.ma2.Index;
+
+/**
+ *
+ * @author mcuif
+ */
+public class RecruitmentStainTracker extends AbstractTracker {
+
+    public RecruitmentStainTracker() {
+        super(DataType.INT);
+    }
+
+    @Override
+    void setDimensions() {
+        addTimeDimension();
+        addDrifterDimension();
+    }
+
+    @Override
+    Array createArray() {
+        return new ArrayInt.D2(1, dimensions().get(1).getLength());
+    }
+
+    @Override
+    public ArrayInt.D2 getArray() {
+        return (ArrayInt.D2) super.getArray();
+    }
+
+    public void track() {
+        IBasicParticle particle;
+        IRecruitableParticle rparticle;
+        Iterator<IBasicParticle> iter = getSimulationManager().getSimulation().getPopulation().iterator();
+        while (iter.hasNext()) {
+            particle = iter.next();
+            rparticle = (IRecruitableParticle) particle.getLayer(RecruitableParticleLayer.class);
+            getArray().set(0, particle.getIndex(), rparticle.isRecruited() ? 1 : 0);
+        }
+    }
+
+}
