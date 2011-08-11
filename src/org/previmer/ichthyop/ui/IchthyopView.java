0061d064d70dfe877414d2063ffd70a845524441
mj1f94e <mj1f94e@example.com>
2011-08-11 12:35:18 +0000

    [IchthyopView.java] to open the configuration file, user can open directly the last directory that he used the last time.

--- src/org/previmer/ichthyop/ui/IchthyopView.java
+++ src/org/previmer/ichthyop/ui/IchthyopView.java
@@ -727,6 +727,8 @@ public class IchthyopView extends FrameView
         int returnPath = chooser.showOpenDialog(getFrame());
         if (returnPath == JFileChooser.APPROVE_OPTION) {
             checkCfgVersionAndLoad(chooser.getSelectedFile());
+            cfgPath=chooser.getCurrentDirectory();
+
         }
         return null;
     }
