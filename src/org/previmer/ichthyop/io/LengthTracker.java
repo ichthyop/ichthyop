97e3ce26dadb90ee4c611cda3ab647b2b42a267e
ga07972 <ga07972@example.com>
2012-08-10 15:34:39 +0000

    (Etracker.java ETracker.propreties LengthTracker.java StageTracker.java) Trackers able to run with DEB E is the particle energy reserve

--- src/org/previmer/ichthyop/io/LengthTracker.java
+++ src/org/previmer/ichthyop/io/LengthTracker.java
@@ -19,6 +19,7 @@ package org.previmer.ichthyop.io;
 
 import org.previmer.ichthyop.arch.IBasicParticle;
 import java.util.Iterator;
+import org.previmer.ichthyop.particle.DebParticleLayer;
 import org.previmer.ichthyop.particle.GrowingParticleLayer;
 import ucar.ma2.Array;
 import ucar.ma2.ArrayFloat;
@@ -43,10 +44,21 @@ public class LengthTracker extends AbstractTracker {
     public void track() {
         IBasicParticle particle;
         Iterator<IBasicParticle> iter = getSimulationManager().getSimulation().getPopulation().iterator();
+        boolean isLinearGrowth=false;
+        if (getSimulationManager().getActionManager().isEnabled("action.growth")){
+                isLinearGrowth=true;
+            }
         while (iter.hasNext()) {
             particle = iter.next();
-            GrowingParticleLayer gParticle = (GrowingParticleLayer) particle.getLayer(GrowingParticleLayer.class);
-            getArray().set(0, particle.getIndex(), (float) gParticle.getLength());
+            if (isLinearGrowth){
+                GrowingParticleLayer gParticle = (GrowingParticleLayer) particle.getLayer(GrowingParticleLayer.class);
+                getArray().set(0, particle.getIndex(), (float) gParticle.getLength());
+            }
+            else {
+                DebParticleLayer gParticle = (DebParticleLayer) particle.getLayer(DebParticleLayer.class);
+                getArray().set(0, particle.getIndex(), (float) gParticle.getLength());
+            }
+
         }
     }
 
