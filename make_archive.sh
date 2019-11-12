dc94b5d31924d6b72aa453f05c891dba2c71f795
nb22522 <nicolas.barrier@ird.fr>
2019-11-12 16:25:11 +0000

    Update in the archive script

--- make_archive.sh
+++ make_archive.sh
@@ -5,6 +5,7 @@ cp -prfv input ${dirin}
 cp -prfv dist/*.jar ${dirin}
 cp -prfv dist/lib ${dirin}
 cp -prfv cfg ${dirin}
+chmod 755 ${dirin}/dist/*.jar
 
-tar -cvf ${dirin}.tar ${dirin}
+zip -r ${dirin}.zip ${dirin}
 rm -rfv ${dirin}
