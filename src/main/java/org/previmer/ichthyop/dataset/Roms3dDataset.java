014deca1a94dfe7828478ee3c5273a52f97b671c
barrier <nicolas.barrier@ird.fr>
2020-10-22 16:09:22 +0200

    Adding method to save W computed from Roms

--- src/main/java/org/previmer/ichthyop/dataset/Roms3dDataset.java
+++ src/main/java/org/previmer/ichthyop/dataset/Roms3dDataset.java
@@ -41,21 +41,33 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  * 
  */
-
 package org.previmer.ichthyop.dataset;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import java.util.logging.Level;
+import static org.previmer.ichthyop.SimulationManagerAccessor.getLogger;
 import org.previmer.ichthyop.event.NextStepEvent;
 import org.previmer.ichthyop.io.IOTools;
 import static org.previmer.ichthyop.io.IOTools.isDirectory;
 import static org.previmer.ichthyop.io.IOTools.isFile;
+import ucar.ma2.Array;
+import ucar.ma2.ArrayFloat;
+import ucar.ma2.DataType;
+import ucar.ma2.Index;
+import ucar.ma2.InvalidRangeException;
+import ucar.nc2.Dimension;
+import ucar.nc2.NetcdfFile;
+import ucar.nc2.dataset.NetcdfDataset;
+import ucar.nc2.NetcdfFileWriteable;
+
 /**
  *
  * @author pverley
  */
 public class Roms3dDataset extends Roms3dCommon {
-    
+
     private List<String> ncfiles;
     private int ncindex;
 
@@ -96,7 +108,7 @@ public class Roms3dDataset extends Roms3dCommon {
         }
         ncIn = DatasetUtil.openFile(ncfiles.get(0), true);
         readTimeLength();
-        
+
         try {
             if (!getParameter("grid_file").isEmpty()) {
                 String path = IOTools.resolveFile(getParameter("grid_file"));  // barrier.n
@@ -104,17 +116,19 @@ public class Roms3dDataset extends Roms3dCommon {
                     throw new IOException("{Dataset} " + getParameter("grid_file") + " is not a valid file.");
                 }
                 gridFile = path;
-            
+
             } else {
                 gridFile = ncIn.getLocation();
             }
         } catch (NullPointerException ex) {
             gridFile = ncIn.getLocation();
-        }      
+        }
+
     }
 
     @Override
     void setOnFirstTime() throws Exception {
+        this.saveWFiles();
         double t0 = getSimulationManager().getTimeManager().get_tO();
         ncindex = DatasetUtil.index(ncfiles, t0, timeArrow(), strTime);
         ncIn = DatasetUtil.openFile(ncfiles.get(ncindex), true);
@@ -122,4 +136,160 @@ public class Roms3dDataset extends Roms3dCommon {
         rank = DatasetUtil.rank(t0, ncIn, strTime, timeArrow());
         time_tp1 = t0;
     }
+
+    public void saveWFiles() throws IOException, InvalidRangeException {
+
+        // Init netcdf variables for reading
+        int[] origin = new int[]{0, 0, jpo, ipo};
+        int[] countU = new int[]{1, nz, ny, (nx - 1)};
+        int[] countV = new int[]{1, nz, ny - 1, nx};
+        int[] countZeta = new int[]{1, ny, nx};
+        int[] originZeta = new int[]{0, jpo, ipo};
+        
+        
+        // Init outputs and output dimension
+        float[][][] w;
+        int[] originOut = new int[]{1, 0, 0, 0};
+
+        for (String f : this.ncfiles) {
+            
+            System.out.println("++++++++++ Processing file " + f);
+
+            // Reconstruction of output filename
+            File fin = new File(f);
+            String direc = fin.getParent();
+            String baseName = fin.getName().replace(".nc", "_reconW.nc");
+            File fout = new File(direc, baseName);
+            
+            // Creation of file output + definition of dims/vars
+            NetcdfFileWriteable ncOut = NetcdfFileWriteable.createNew(fout.getAbsolutePath());
+            Dimension timeDim = ncOut.addUnlimitedDimension("time");
+            Dimension xDim = ncOut.addDimension("x", nx);
+            Dimension yDim = ncOut.addDimension("y", ny);
+            Dimension zDim = ncOut.addDimension("z", nz + 1);
+            Dimension[] dimArr = {timeDim, zDim, yDim, xDim};
+            Dimension[] dimArrZeta = {timeDim, yDim, xDim};
+                       
+            ncOut.addVariable("U", DataType.FLOAT, dimArr);
+            ncOut.addVariable("V", DataType.FLOAT, dimArr);
+            ncOut.addVariable("W", DataType.FLOAT, dimArr);
+            ncOut.addVariable("Zeta", DataType.FLOAT, dimArrZeta);
+            ncOut.addVariable("Depth", DataType.FLOAT, dimArr);
+            ncOut.create();
+
+            // Opening of NetCDF file and reading of U/V/Zeta
+            NetcdfFile nc;
+            nc = NetcdfDataset.openDataset(f);
+            
+            // Correct the time counter to read all variables
+            int ntime = nc.findDimension("time").getLength();
+            
+            // Update the counter of U/V/Zeta
+            countU[0] = ntime;
+            countV[0] = ntime;
+            countZeta[0] = ntime;
+            
+            // Read all
+            Array arrU = ncIn.findVariable(strU).read(origin, countU).reduce();
+            Array arrV = ncIn.findVariable(strV).read(origin, countV).reduce();
+            Array arrZeta = ncIn.findVariable(strZeta).read(originZeta, countZeta).reduce();
+            Index index;
+                
+            nc.close();
+            
+            // Loop over all the time variables
+            for (int t = 0; t < ntime; t++) {
+                
+                System.out.println("time index " + t);
+                
+                // Read Zeta
+                index = arrZeta.getIndex();
+                zeta_tp1 = new float[ny][nx];
+                for (int j = 0; j < ny; j++) {
+                    for (int i = 0; i < nx; i++) {
+                        zeta_tp1[j][i] = arrZeta.getFloat(index.set(t, j, i));
+                    }
+                }
+                
+                // Read U
+                u_tp1 = new float[nz][ny][nx - 1];
+                index = arrU.getIndex();
+                for (int k = 0; k < nz; k++) {
+                    for (int j = 0; j < ny; j++) {
+                        for (int i = 0; i < nx - 1; i++) {
+                            u_tp1[k][j][i] = arrU.getFloat(index.set(t, k, j, i));
+                        }
+                    }
+                }
+
+                // Read V
+                v_tp1 = new float[nz][ny - 1][nx];
+                index = arrV.getIndex();
+                for (int k = 0; k < nz; k++) {
+                    for (int j = 0; j < ny - 1; j++) {
+                        for (int i = 0; i < nx; i++) {
+                            v_tp1[k][j][i] = arrV.getFloat(index.set(t, k, j, i));
+                        }
+                    }
+                }
+
+                // Computes Z(t, z, y, x)
+                z_w_tp1 = this.getSigLevels(); //[nz + 1][ny][nx];
+                
+                // Compute W
+                w = this.computeW();  // [nz + 1][ny][nx];
+
+                // Prepare arrays for writting
+                ArrayFloat.D4 arrayW = new ArrayFloat.D4(1, nz + 1, ny, nx);
+                for (int k = 0; k < nz; k++) {
+                    for (int j = 0; j < ny; j++) {
+                        for (int i = 0; i < nx - 1; i++) {
+                            arrayW.set(0, k, j, i, (float) w[k][j][i]);
+                        }
+                    }
+                }
+                
+                ArrayFloat.D4 arrayZ = new ArrayFloat.D4(1, nz + 1, ny, nx);
+                for (int k = 0; k < nz; k++) {
+                    for (int j = 0; j < ny; j++) {
+                        for (int i = 0; i < nx - 1; i++) {
+                            arrayZ.set(0, k, j, i, (float) z_w_tp1[k][j][i]);
+                        }
+                    }
+                }
+
+                ArrayFloat.D4 arrayU = new ArrayFloat.D4(1, nz + 1, ny, nx);
+                for (int k = 0; k < nz; k++) {
+                    for (int j = 0; j < ny; j++) {
+                        for (int i = 0; i < nx - 1; i++) {
+                            arrayU.set(0, k, j, i, u_tp1[k][j][i]);
+                        }
+                    }
+                }
+                
+                ArrayFloat.D4 arrayV = new ArrayFloat.D4(1, nz + 1, ny, nx);
+                for (int k = 0; k < nz; k++) {
+                    for (int j = 0; j < ny - 1; j++) {
+                        for (int i = 0; i < nx; i++) {
+                            arrayV.set(0, k, j, i, v_tp1[k][j][i]);
+                        }
+                    }
+                }
+                
+                originOut[0] = t;
+                ncOut.write("U", originOut, arrayU);
+                ncOut.write("V", originOut, arrayV);
+                ncOut.write("Depth", originOut, arrayZ);
+                ncOut.write("W", originOut, arrayW);
+                
+            }  // end of time loop
+
+            ncOut.close();
+            
+        }  // end of ncfiles loop
+
+        System.exit(0);
+
+    }  // end of method
+
 }
