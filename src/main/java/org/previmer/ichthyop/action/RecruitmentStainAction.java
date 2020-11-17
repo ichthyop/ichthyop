8980a703a6df9d94dca408ac03cc21c9d4cd1f1f
barriern <nicolas.barrier@ird.fr>
2020-11-17 13:16:42 +0100

    Format + remove unused + use <>

--- src/main/java/org/previmer/ichthyop/action/RecruitmentStainAction.java
+++ src/main/java/org/previmer/ichthyop/action/RecruitmentStainAction.java
@@ -1,18 +1,18 @@
-/* 
- * 
+/*
+ *
  * ICHTHYOP, a Lagrangian tool for simulating ichthyoplankton dynamics
  * http://www.ichthyop.org
- * 
+ *
  * Copyright (C) IRD (Institut de Recherce pour le Developpement) 2006-2020
  * http://www.ird.fr
- * 
+ *
  * Main developper: Philippe VERLEY (philippe.verley@ird.fr), Nicolas Barrier (nicolas.barrier@ird.fr)
  * Contributors (alphabetically sorted):
  * Gwendoline ANDRES, Sylvain BONHOMMEAU, Bruno BLANKE, Timothée BROCHIER,
  * Christophe HOURDIN, Mariem JELASSI, David KAPLAN, Fabrice LECORNU,
  * Christophe LETT, Christian MULLON, Carolina PARADA, Pierrick PENVEN,
  * Stephane POUS, Nathan PUTMAN.
- * 
+ *
  * Ichthyop is a free Java tool designed to study the effects of physical and
  * biological factors on ichthyoplankton dynamics. It incorporates the most
  * important processes involved in fish early life: spawning, movement, growth,
@@ -20,36 +20,33 @@
  * temperature and salinity fields archived from oceanic models such as NEMO,
  * ROMS, MARS or SYMPHONIE. It runs with a user-friendly graphic interface and
  * generates output files that can be post-processed easily using graphic and
- * statistical software. 
- * 
+ * statistical software.
+ *
  * To cite Ichthyop, please refer to Lett et al. 2008
  * A Lagrangian Tool for Modelling Ichthyoplankton Dynamics
  * Environmental Modelling & Software 23, no. 9 (September 2008) 1210-1214
  * doi:10.1016/j.envsoft.2008.02.005
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation (version 3 of the License). For a full 
+ * the Free Software Foundation (version 3 of the License). For a full
  * description, see the LICENSE file.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- * 
+ *
  */
 
 package org.previmer.ichthyop.action;
 
-import org.previmer.ichthyop.Simulation;
 import org.previmer.ichthyop.util.Constant;
 import org.previmer.ichthyop.particle.RecruitableParticleLayer;
-
 import org.previmer.ichthyop.particle.IParticle;
-import org.previmer.ichthyop.dataset.DatasetUtil;
 import org.previmer.ichthyop.dataset.DistanceGetter;
 import org.previmer.ichthyop.io.RecruitmentStainTracker;
 import org.previmer.ichthyop.manager.SimulationManager;
@@ -68,7 +65,8 @@ public class RecruitmentStainAction extends AbstractAction {
      * Minimum duration [second] a particle has to spend within the same zone
      * before being recruited.
      */
-    private static int durationMinInRecruitArea;
+    //private static int durationMinInRecruitArea;
+
     /**
      * Duration [second] presently spent by the particule within the
      * current zone.
@@ -81,7 +79,7 @@ public class RecruitmentStainAction extends AbstractAction {
     private double radius_stain;
     private double thickness_stain;
     private boolean is3D;
-    private static final double ONE_DEG_LATITUDE_IN_METER = 111138.d;
+    //private static final double ONE_DEG_LATITUDE_IN_METER = 111138.d;
 
     @Override
     public void loadParameters() throws Exception {
@@ -116,7 +114,7 @@ public class RecruitmentStainAction extends AbstractAction {
             getSimulationManager().getOutputManager().addPredefinedTracker(RecruitmentStainTracker.class);
         }
     }
-    
+
     @Override
     public void init(IParticle particle) {
         // Nothing to do
