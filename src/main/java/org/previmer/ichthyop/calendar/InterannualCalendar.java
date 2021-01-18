c7bc44a31795cf3cf075ff1c8daedff35f44f40f
barriern <nicolas.barrier@ird.fr>
2021-01-18 16:41:52 +0100

    Adding constructor

--- src/main/java/org/previmer/ichthyop/calendar/InterannualCalendar.java
+++ src/main/java/org/previmer/ichthyop/calendar/InterannualCalendar.java
@@ -44,20 +44,24 @@
 
 package org.previmer.ichthyop.calendar;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 /** Import the abstract class Calendar */
 import java.util.Calendar;
 
 /**
- * <p>The class extends the abstact class {@link java.util.Calendar}. It provides
- * methods for converting between a specific instant in time and a set of
- * fields such as <code>YEAR</code>, <code>MONTH</code>,
- * <code>DAY_OF_MONTH</code>, <code>HOUR</code>, and so on, according with the
- * Gregorian calendar. An instant in time is represented by a millisecond
- * value that is, by default, an offset from January 1, 1900 00:00:00.000 GMT.
- * The origin can be set by the user in one of the constructors.</p>
+ * <p>
+ * The class extends the abstact class {@link java.util.Calendar}. It provides
+ * methods for converting between a specific instant in time and a set of fields
+ * such as <code>YEAR</code>, <code>MONTH</code>, <code>DAY_OF_MONTH</code>,
+ * <code>HOUR</code>, and so on, according with the Gregorian calendar. An
+ * instant in time is represented by a millisecond value that is, by default, an
+ * offset from January 1, 1900 00:00:00.000 GMT. The origin can be set by the
+ * user in one of the constructors.
+ * </p>
  * The class is a very simplified version of the Gregorian Calendar, except that
- * the epoch is not automatically set to January 1, 1970 (Gregorian),
- * midnight UTC.
+ * the epoch is not automatically set to January 1, 1970 (Gregorian), midnight
+ * UTC.
  *
  * @author P.Verley 2007
  * @see java.util.Calendar
@@ -70,41 +74,45 @@ public class InterannualCalendar extends Calendar {
     	 */
     private static final long serialVersionUID = 6855081607747702909L;
     ///////////////////////////////
-// Declaration of the variables
-///////////////////////////////
+    // Declaration of the variables
+    ///////////////////////////////
     /**
      * Origin of time
      */
     final private int[] epoch_fields;
-///////////////////////////////
-// Declaration of the constants
-///////////////////////////////
+    ///////////////////////////////
+    // Declaration of the constants
+    ///////////////////////////////
     public static final int ONE_SECOND = 1000;
     public static final int ONE_MINUTE = 60 * ONE_SECOND;
     public static final int ONE_HOUR = 60 * ONE_MINUTE;
     public static final long ONE_DAY = 24 * ONE_HOUR;
     public static final long ONE_WEEK = 7 * ONE_DAY;
-    private static final int NUM_DAYS[] = {0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334}; // 0-based, for day-in-year
-    private static final int LEAP_NUM_DAYS[] = {0, 31, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335}; // 0-based, for day-in-year
+    private static final int NUM_DAYS[] = { 0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334 }; // 0-based, for
+                                                                             // day-in-year
+    private static final int LEAP_NUM_DAYS[] = { 0, 31, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335 }; // 0-based,
+                                                                                                          // for
+                                                                                                          // day-in-year
 
-///////////////
-// Constructors
-///////////////
+    ///////////////
+    // Constructors
+    ///////////////
     /**
-     * Constructs a Gregorian Calendar with origin of time
-     * January 1, 1900 00:00:00.000 GMT.
+     * Constructs a Gregorian Calendar with origin of time January 1, 1900
+     * 00:00:00.000 GMT.
      */
     public InterannualCalendar() {
         this(1582, OCTOBER, 15, 0, 0);
     }
 
     /**
-     * Constructs a Gregorian Calendar with origin of time set by parameters.
-     * Hours, minutes, seconds are automatically set to 00:00:00.000
-     * @param year an int, the year origin
-     * @param month an int, the month origin
-     * @param day an int, the day origin
-     * @param hour an int, the hour origin
+     * Constructs a Gregorian Calendar with origin of time set by parameters. Hours,
+     * minutes, seconds are automatically set to 00:00:00.000
+     * 
+     * @param year   an int, the year origin
+     * @param month  an int, the month origin
+     * @param day    an int, the day origin
+     * @param hour   an int, the hour origin
      * @param minute an int, the minute origin
      */
     public InterannualCalendar(int year, int month, int day, int hour, int minute) {
@@ -120,9 +128,28 @@ public class InterannualCalendar extends Calendar {
         setTimeInMillis(0);
     }
 
-////////////////////////////
-// Definition of the methods
-////////////////////////////
+    public InterannualCalendar(String time_origin, SimpleDateFormat INPUT_DATE_FORMAT) throws ParseException {
+        Calendar calendar_o = Calendar.getInstance();
+        calendar_o.setTime(INPUT_DATE_FORMAT.parse(time_origin));
+        int year_o = calendar_o.get(Calendar.YEAR);
+        int month_o = calendar_o.get(Calendar.MONTH);
+        int day_o = calendar_o.get(Calendar.DAY_OF_MONTH);
+        int hour_o = calendar_o.get(Calendar.HOUR_OF_DAY);
+        int min_o = calendar_o.get(Calendar.MINUTE);
+        epoch_fields = new int[FIELD_COUNT];
+        fields = new int[FIELD_COUNT];
+        setEpoch(YEAR, year_o);
+        setEpoch(MONTH, month_o);
+        setEpoch(DAY_OF_MONTH, day_o);
+        setEpoch(HOUR_OF_DAY, hour_o);
+        setEpoch(MINUTE, min_o);
+        setEpoch(SECOND, 0);
+        setTimeInMillis(0);
+    }
+
+    ////////////////////////////
+    // Definition of the methods
+    ////////////////////////////
     /**
      * Sets the given epoch field (origin of time) to the given value.
      *
@@ -163,22 +190,18 @@ public class InterannualCalendar extends Calendar {
         boolean isLeap;
         long timeInDay = millisToDay(time);
 
-        long timeInDay_o = timeInDay + (isLeap(epoch_fields[YEAR])
-                ? LEAP_NUM_DAYS[epoch_fields[MONTH]] + epoch_fields[DAY_OF_MONTH] - 1
-                : NUM_DAYS[epoch_fields[MONTH]] + epoch_fields[DAY_OF_MONTH] - 1);
+        long timeInDay_o = timeInDay
+                + (isLeap(epoch_fields[YEAR]) ? LEAP_NUM_DAYS[epoch_fields[MONTH]] + epoch_fields[DAY_OF_MONTH] - 1
+                        : NUM_DAYS[epoch_fields[MONTH]] + epoch_fields[DAY_OF_MONTH] - 1);
 
         long nbDays = 0;
         int currentYear = epoch_fields[YEAR];
         while (nbDays < timeInDay_o) {
-            nbDays += isLeap(currentYear)
-                    ? 366
-                    : 365;
+            nbDays += isLeap(currentYear) ? 366 : 365;
             currentYear++;
         }
         --currentYear;
-        nbDays -= isLeap(currentYear)
-                ? 366
-                : 365;
+        nbDays -= isLeap(currentYear) ? 366 : 365;
         rawYear = (int) currentYear;
         dayOfYear = (int) (timeInDay_o - nbDays);
         isLeap = isLeap(rawYear);
@@ -221,8 +244,8 @@ public class InterannualCalendar extends Calendar {
     }
 
     /**
-     * Determines if the given year is a leap year. Returns true if the
-     * given year is a leap year.
+     * Determines if the given year is a leap year. Returns true if the given year
+     * is a leap year.
      *
      * @param year the given year.
      * @return true if the given year is a leap year; false otherwise.
@@ -242,34 +265,26 @@ public class InterannualCalendar extends Calendar {
         boolean isLeap = isLeap(yearOn);
         long time2Day = (long) (fields[DAY_OF_MONTH] - 1);
         try {
-            time2Day += isLeap
-                    ? (long) (LEAP_NUM_DAYS[monthOn])
-                    : (long) (NUM_DAYS[monthOn]);
+            time2Day += isLeap ? (long) (LEAP_NUM_DAYS[monthOn]) : (long) (NUM_DAYS[monthOn]);
         } catch (Exception ex) {
         }
 
         for (int incYear = epoch_fields[YEAR]; incYear < yearOn; incYear++) {
-            time2Day += isLeap(incYear)
-                    ? 366
-                    : 365;
+            time2Day += isLeap(incYear) ? 366 : 365;
         }
 
         time2Day -= (epoch_fields[DAY_OF_MONTH] - 1);
-        time2Day -= isLeap(epoch_fields[YEAR])
-                ? LEAP_NUM_DAYS[epoch_fields[MONTH]]
-                : NUM_DAYS[epoch_fields[MONTH]];
+        time2Day -= isLeap(epoch_fields[YEAR]) ? LEAP_NUM_DAYS[epoch_fields[MONTH]] : NUM_DAYS[epoch_fields[MONTH]];
 
         long millis = dayToMillis(time2Day);
         int millisInDay = fields[MILLISECOND]
-                + 1000
-                * (fields[SECOND]
-                + 60 * (fields[MINUTE] + 60 * fields[HOUR_OF_DAY]));
+                + 1000 * (fields[SECOND] + 60 * (fields[MINUTE] + 60 * fields[HOUR_OF_DAY]));
         time = millis + millisInDay;
     }
 
-//////////////////////////////////
-// Inherited methods not redefined
-//////////////////////////////////
+    //////////////////////////////////
+    // Inherited methods not redefined
+    //////////////////////////////////
     @Override
     public int getGreatestMinimum(int field) {
         return 0;
@@ -297,5 +312,5 @@ public class InterannualCalendar extends Calendar {
     @Override
     public void roll(int field, boolean up) {
     }
-    //---------- End of class
+    // ---------- End of class
 }
