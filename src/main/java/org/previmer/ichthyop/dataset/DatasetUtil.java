cac3d90c3924c5f36c9155369c1d4446f746951a
barriern <nicolas.barrier@ird.fr>
2021-06-01 09:21:34 +0200

    remove possible "t" string in time units

--- src/main/java/org/previmer/ichthyop/dataset/DatasetUtil.java
+++ src/main/java/org/previmer/ichthyop/dataset/DatasetUtil.java
@@ -464,9 +464,10 @@ public class DatasetUtil {
         int beginIndex = units.indexOf("since") + 5;
         int endIndex = units.lastIndexOf(":");
         String subUnits = units.substring(beginIndex, endIndex).trim();
+        subUnits = subUnits.replace("t", " ");
         dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
         LocalDateTime dateUnit = LocalDateTime.parse(subUnits, dateFormatter);
-
+            
         if (units.contains("second")) {
             finalDate = dateUnit.plusSeconds(time);
         } else if (units.contains("hour")) {
