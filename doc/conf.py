79318bbab8397b330018feab5406c6405819ec2a
Nicolas Barrier <nicolas.barrier@ird.fr>
2023-10-09 15:15:34 +0200

    Adding Ichthyop logo

--- doc/conf.py
+++ doc/conf.py
@@ -118,6 +118,7 @@ bibtex_bibfiles = ['_static/biblio.bib']
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_logo = '_static/logo-ichthyop.svg'
 
 # use figure numbers for referencing figures
 numfig = True
