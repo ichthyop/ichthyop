94171658f4c23498e4f55d1bbd170b2e78938606
Nicolas Barrier <nicolas.barrier@ird.fr>
2024-01-18 12:08:33 +0100

    Update pom and scene size

--- src/main/java/org/previmer/ichthyop/ui/JavaFXIchthyop.java
+++ src/main/java/org/previmer/ichthyop/ui/JavaFXIchthyop.java
@@ -17,7 +17,7 @@ public class JavaFXIchthyop extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("JavaFXIchthyop"), 640, 480);
+        scene = new Scene(loadFXML("JavaFXIchthyop"));
         stage.setScene(scene);
         stage.show();
     }
