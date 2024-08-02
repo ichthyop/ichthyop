b05c22113527cf002199d301ca23a041c41d4db9
Nicolas Barrier <nicolas.barrier@ird.fr>
2024-08-02 10:23:26 +0200

    Adding a class for velocity computation

---
+++ src/main/java/org/previmer/ichthyop/action/orientation/OrientationVelocity.java
@@ -0,0 +1,57 @@
+package org.previmer.ichthyop.action.orientation;
+
+import java.util.logging.Level;
+
+import org.previmer.ichthyop.action.AbstractAction;
+import org.previmer.ichthyop.particle.IParticle;
+
+public abstract class OrientationVelocity extends AbstractAction {
+
+    private double swimmingSpeedHatch;
+    private double swimmingSpeedSettle;
+    private double secs_in_day = 86400;
+    double PLD;
+
+    public interface InnerOrientationVelocity {
+        double getVelocity(IParticle particle);
+    }
+
+    private InnerOrientationVelocity velocityMethod;
+
+    @Override
+    public void loadParameters() throws Exception {
+        swimmingSpeedHatch = Double.valueOf(getParameter("swimming.speed.hatch"));
+        swimmingSpeedSettle = Double.valueOf(getParameter("swimming.speed.settle"));
+
+        if (swimmingSpeedHatch > swimmingSpeedSettle) {
+            getLogger().log(Level.WARNING, "Hatch and Settle velocity have been swapped");
+            double temp = swimmingSpeedHatch;
+            swimmingSpeedHatch = swimmingSpeedSettle;
+            swimmingSpeedSettle = temp;
+        }
+
+        double timeMax = getSimulationManager().getTimeManager().getSimulationDuration();
+        PLD = timeMax / (secs_in_day);
+        velocityMethod = (IParticle particle) -> getVelocityPLD(particle);
+
+    }
+
+    @Override
+    public void init(IParticle particle) {
+
+    }
+
+    public double getVvelocityMethodelocity(IParticle particle) {
+        return velocityMethod.getVelocity(particle);
+    }
+
+    public double getVelocityPLD(IParticle particle) {
+
+        double age = particle.getAge() / (secs_in_day) + Float.MIN_VALUE;
+        double swimmingSpeed = swimmingSpeedHatch + Math.pow(10,
+                (Math.log10(age) / Math.log10(PLD)) * Math.log10(swimmingSpeedSettle - swimmingSpeedHatch));
+        swimmingSpeed = swimmingSpeed / 100;
+        return swimmingSpeed;
+    }
+
+}
