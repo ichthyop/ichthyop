91f602c2146ef148cc18e75a19e9fbc9e18a90b0
Nicolas Barrier <nicolas.barrier@ird.fr>
2025-07-04 10:03:08 +0200

    Change the way to manage age.min and age.max

--- src/main/java/org/previmer/ichthyop/action/orientation/ReefOrientationAction.java
+++ src/main/java/org/previmer/ichthyop/action/orientation/ReefOrientationAction.java
@@ -39,13 +39,13 @@ public class ReefOrientationAction extends OrientationVelocity {
         maximumDistance = Double.valueOf(getParameter("maximum.distance"));
 
         // Provides age in days
-        if(getParameter("age.min") != null) {
+        if(!isNull("age.min")) {
             ageMin = Double.valueOf(getParameter("age.min"));
         } else {
             ageMin = 0;
         }
 
-        if(getParameter("age.max") != null) {
+        if(!isNull("age.max")) {
             ageMax = Double.valueOf(getParameter("age.max"));
         } else {
             ageMax = Double.MAX_VALUE;
