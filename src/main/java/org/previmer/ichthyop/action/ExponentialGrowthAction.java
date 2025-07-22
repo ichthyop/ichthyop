03d4a771aa01b9f014f9df4fa8708595b20f4422
Nicolas Barrier <nicolas.barrier@ird.fr>
2025-07-22 16:06:20 +0200

    Adding a test on whether particle is locked or not

--- src/main/java/org/previmer/ichthyop/action/ExponentialGrowthAction.java
+++ src/main/java/org/previmer/ichthyop/action/ExponentialGrowthAction.java
@@ -105,6 +105,12 @@ public class ExponentialGrowthAction extends AbstractAction {
 
     @Override
     public void execute(IParticle particle) {
+
+        if(particle.isLocked()) {
+            // Do not grow locked particles
+            return;
+        }
+
         LengthParticleLayer sole = (LengthParticleLayer) particle.getLayer(LengthParticleLayer.class);
         double temp = getSimulationManager().getDataset().get(temperature_field, sole.particle().getGridCoordinates(), getSimulationManager().getTimeManager().getTime()).doubleValue();
         sole.incrementLength(grow(temp, sole.getLength()));
