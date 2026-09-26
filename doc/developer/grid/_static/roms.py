1e163cf4711312c469c22e5113a04cf8dbddd13a
Nicolas Barrier <nicolas.barrier@ird.fr>
2026-09-26 17:15:54 +0200

    Update roms config

--- doc/developer/grid/_static/roms.py
+++ doc/developer/grid/_static/roms.py
@@ -6,7 +6,7 @@
 #       extension: .py
 #       format_name: light
 #       format_version: '1.5'
-#       jupytext_version: 1.13.8
+#       jupytext_version: 1.15.1
 #   kernelspec:
 #     display_name: Python 3 (ipykernel)
 #     language: python
@@ -21,7 +21,16 @@
 #
 # <img src=https://www.myroms.org/wiki/images/0/0f/staggered_grid_rho_cells.png>
 #
-# Contrary to NEMO and MARS, the `U` points are located on the *western* face, while velocities are located on the *southern* faces. However, while the `T` interior domain has $(N_y, N_x)$ dimensions, the `U` domain is $(N_y, N_x - 1)$ while the `V` domain is $(N_y - 1, N_x)$. Indeed, the first elements are discarded. The grid as saved by ROMS is as follows:
+# In the ROMS output files, T points have dimensions of $( N_{Y_T}, N_{X_T})$. U points have dimensions of $(N_{Y_T}, N_{X_U})$, with $N_{X_U} = N_{X_T} - 1$. 
+# V points have dimensions of $(N_{Y_V}, N_{X_T})$, with $N_{Y_V} = N_{Y_T} - 1$. In Ichthyop, the tracer and velocity fields will be extracted on the **inner** domain (the gray area in the above). As a consequence, the data extraction will be as follows:
+#
+# - For T points: `start = [1, 1]`, `count = [ny - 2, nx -2]`, i.e. we remove the T points outside of the inner domain
+# - For U points: `start = [1, 0]`, `count = [ny - 2, nx -1]`, i.e. we remove the U points outside of the inner domain (northernmost and southernmost cells), while keeping velocity at the westernmost and easternmost edges of the inner domain.
+# - For V points: `start = [1, 1]`, `count = [ny - 2, nx -2]`, i.e. we remove the V points outside of the inner domain (northernmost and southernmost cells), while keeping velocity at the northernmost and southernmost edges of the inner domain.
+#
+# Consequently, at the end of this data extraction, from an Ichthyop perspective, there will be an extra U point on the X dimension, and one extra V point on the Y dimension. The final layout will therefore be as follows.
+#
+# <!-- Contrary to NEMO and MARS, the `U` points are located on the *western* face, while velocities are located on the *southern* faces. However, while the `T` interior domain has $(N_y, N_x)$ dimensions, the `U` domain is $(N_y, N_x - 1)$ while the `V` domain is $(N_y - 1, N_x)$. Indeed, the first elements are discarded. The grid as saved by ROMS is as follows: -->
 
 # +
 import os
@@ -53,34 +62,46 @@ ax = plt.gca()
 ax.set_aspect('equal', 'box')
 plt.xlim(0, xmax)
 plt.ylim(0, ymax)
-ax.set_xticks(np.arange(0, xmax + 1), minor=False)
-ax.set_yticks(np.arange(0, ymax + 1), minor=False)
+ax.set_xticks(np.arange(0, xmax + 1) - 0.5, minor=False)
+ax.set_yticks(np.arange(0, ymax + 1) - 0.5, minor=False)
 plt.grid(True, linewidth=2, linestyle='--', color='k')
 bbox['fc'] = 'orange'
 for x in range(0, xmax):
     for y in range(0, ymax):
         #lt = plt.plot(x + 0.5, y + 0.5, marker='.', color='g')
-        lt = plt.text(x + 0.5, y + 0.5, '%d,%d' %(x, y), bbox=bbox, ha='center', va='center', color='k', zorder=100)
+        lt = plt.text(x, y, '%d,%d' %(x, y), bbox=bbox, ha='center', va='center', color='k', zorder=100)
 
+# Plotting U points
 bbox['fc'] = 'firebrick'
 bbox['alpha'] = 1
-for x in np.arange(xmax - 1):
+for x in np.arange(xmax + 1):
     for y in range(ymax):
         #lu = plt.plot(x - 0.5, y, marker='.', color='r')
-        lu = plt.text(x + 1, y + 0.5, '%d,%d' %(x,y), bbox=bbox, ha='center', va='center', color='w', zorder=100)
+        lu = plt.text(x - 0.5, y, '%d,%d' %(x,y), bbox=bbox, ha='center', va='center', color='w', zorder=100)
 
+# Plotting V points
 bbox['fc'] = 'powderblue'
 for x in np.arange(0, xmax):
-    for y in range(ymax - 1):
+    for y in range(ymax + 1):
         #lv = plt.plot(x, y - 0.5, marker='.', color='b')
-        lv = plt.text(x + 0.5, y + 1, '%d,%d' %(x,y), bbox=bbox, ha='center', va='center', color='k')
+        lv = plt.text(x, y - 0.5, '%d,%d' %(x,y), bbox=bbox, ha='center', va='center', color='k')
+
+plt.plot([0, 7, 7, 0, 0], [0, 0, 4, 4, 0], lw=6)
 
 #l = plt.legend([lt, lu, lv], ['T', 'U', 'V'], ncol=1, loc='right', bbox_to_anchor=(1.2, 0.6))
 savefig('ichthyop_grid_roms')
-# -
-# However, since Ichthyop reads the `U` and `V` files starting from index `0`, every looks as follows:
 
-# Consequently, everything is as such the `U` and `V` points are on the northern and eastern faces. Except that the easternmost `T` point has no `U` point on the right, while the northernmost `T` point has no `V` point.
+
+# -
+def plot_grid_layout():
+    ax = plt.gca()
+    plt.plot([0, 7, 7, 0, 0], [0, 0, 4, 4, 0], lw=3, color='gray')
+    ax.set_xticks(np.arange(xmax + 1) - 0.5, minor=False)
+    ax.set_yticks(np.arange(ymax + 1) - 0.5, minor=False)
+    plt.xlim(0 - 0.5, xmax - 0.5)
+    plt.ylim(0 - 0.5, ymax -0.5)
+    plt.grid(True, linewidth=1, color='gray', ls='--')
+
 
 # ### Interpolation
 #
@@ -96,29 +117,30 @@ plt.rcParams['lines.markersize'] = 2
 def plot_points(ix, jy, color):
     print('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@', ix,  jy)
     plt.plot(ix, jy, marker='o', color=color, markersize=6)
-    j = np.floor(jy - 0.5)
-    i = np.floor(ix - 0.5)
+    j = np.floor(jy)
+    i = np.floor(ix)
     cpt = 0
     for ii in range(2):
-        cox = 1 - abs(ix - 0.5 - (i + ii))
+        cox = 1 - abs(ix - (i + ii))
+        # print('cox ', cox, ix, i + ii)
         for jj in range(2):
-            print(i + ii + 0.5, j + jj + 0.5)
-            coy = 1 - abs(jy - 0.5 - (j + jj))
-            print(cox, coy)
-            plt.plot(i + ii + 0.5, j + jj + 0.5, marker=markers[cpt], color=color, markersize=6, linestyle='none')
+            coy = 1 - abs(jy - (j + jj))
+            print('-----')
+            print(f'ix={ix}, i={i+ii}, cox={cox:.2f}')
+            print(f'jy={jy}, j={j+jj}, coy={coy:.2f}')
+            print(f'ti={i + ii}, tj={j + jj}')
+            plt.plot(i + ii, j + jj, marker=markers[cpt], color=color, markersize=6, linestyle='none')
             cpt += 1
 
 plt.figure()
 ax = plt.gca()
-plt.xlim(0, xmax)
-plt.ylim(0, ymax)
-ax.set_xticks(np.arange(xmax + 1), minor=False)
-ax.set_yticks(np.arange(ymax + 1), minor=False)
-plt.grid(True, linewidth=2, color='gray')
-[plt.plot([x + 0.5], [y + 0.5], marker='o', color='k', markersize=4) for x in range(xmax) for y in range(ymax)]
+plot_grid_layout()
+[plt.plot([x], [y], marker='o', color='k', markersize=4) for x in range(xmax) for y in range(ymax)]
 plot_points(5.7, 3.1, 'r')
 plot_points(2.7, 1.7, 'b')
-plot_points(1.2, 4.1, 'g')
+plot_points(1.2, 3.1, 'g')
+# plt.axis('equal')
+ax.set_aspect('equal', adjustable='box')
 plt.title('Interpolation of T variables')
 savefig('interpolation_t_roms')
 # -
@@ -131,32 +153,38 @@ plt.rcParams['lines.markersize'] = 2
 def plot_points(ix, jy, color):
     print('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@', ix,  jy)
     plt.plot(ix, jy, marker='o', color=color, markersize=6)
-    j = np.floor(jy - 0.5)
-    i = np.floor(ix - 1)
+    j = np.floor(jy)
+
+    # i is the index of the u points to select
+    # from U points of view, x is shifted by 0.5
+    # we therefore move ix to the U referential 
+    # therefore, i is the index from the U point of vue.
+    # i = 0 is the outer left U Point
+    i = np.floor(ix + 0.5)  
     cpt = 0
     for ii in range(2):
-        cox = 1 - abs(ix - 1 - (i - ii))
+        # For distance computation, we need to move the i index back to 
+        # the T grid reference in which ix is defined. Hence we shift by 0.5
+        cox = 1 - abs((ix - (i - 0.5 + ii)))
         for jj in range(2):
-            print(i + ii + 1, j + jj + 0.5)
-            coy = 1 - abs(jy - 0.5 - (j + jj))
-            print(cox, coy)
-            plt.plot(i + ii + 1, j + jj + 0.5, marker=markers[cpt], color=color, markersize=6, linestyle='none')
+            coy = 1 - abs((jy - (j + jj)))
+            print('-----')
+            print(f'ix={ix}, i={i+ii - 0.5}, cox={cox:.2f}')
+            print(f'jy={jy}, j={j+jj}, coy={coy:.2f}')
+            print(f'ui={i + ii}, uj={j + jj}')
+            plt.plot(i + ii - 0.5, j + jj , marker=markers[cpt], color=color, markersize=6, linestyle='none')
             cpt += 1
 
 plt.figure()
 ax = plt.gca()
-plt.xlim(0, xmax)
-plt.ylim(0, ymax)
-ax.set_xticks(np.arange(xmax + 1), minor=False)
-ax.set_yticks(np.arange(ymax + 1), minor=False)
-plt.grid(True, linewidth=2, color='gray')
-[plt.plot([x + 1], [y + 0.5], marker='o', color='k', markersize=4) for x in range(xmax - 1) for y in range(ymax)]
+plot_grid_layout()
+[plt.plot([x - 0.5], [y], marker='o', color='k', markersize=4) for x in range(xmax + 1 ) for y in range(ymax)]
 plt.grid(True, which='minor')
-l = ax.set_xticks(np.arange(xmax + 1))
-l = ax.set_yticks(np.arange(ymax + 1))
 plot_points(5.7, 3.1, 'r')
 plot_points(2.7, 1.7, 'b')
-plot_points(1.2, 4.1, 'g')
+plot_points(1.2, 3.1, 'g')
+plot_points(0.2, 3.1, 'g')
+
 plt.title('Interpolation of U variables')
 savefig('interpolation_u_roms')
 # -
@@ -169,29 +197,33 @@ plt.rcParams['lines.markersize'] = 2
 def plot_points(ix, jy, color):
     print('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@', ix,  jy)
     plt.plot(ix, jy, marker='o', color=color, markersize=4)
-    j = np.floor(jy - 1)
-    i = np.floor(ix - 0.5)
+
+    # from a V perspective, we need to move
+    # jy to the V grid layout by shifting by 0.5
+    j = np.floor(jy + 0.5)  # (j, i) is the grid cell index of V point to select (low left)
+    i = np.floor(ix)
     cpt = 0
     for ii in range(2):
-        cox = 1 - abs(ix - 0.5 - (i + ii))
+        cox = 1 - abs(ix - (i + ii))
         for jj in range(2):
-            print(i + ii + 0.5, j + jj + 1)
-            coy = 1 - abs(jy - 1 - (j + jj))
-            print(cox, coy)
-            plt.plot(i + ii + 0.5, j + jj + 1, marker=markers[cpt], color=color, markersize=6, linestyle='none')
+            # For distance computation, we need to move the j index back to 
+            # the T grid reference in which jy is defined. Hence we shift by 0.5
+            print('-----')
+            coy = 1 - abs(jy - (j - 0.5 + jj))
+            print(f'ix={ix}, i={i+ii}, cox={cox:.2f}')
+            print(f'jy={jy}, j={j+jj}, coy={coy:.2f}')
+            print(f'vi={i + ii}, vj={j + jj}')
+            plt.plot(i + ii, j + jj - 0.5, marker=markers[cpt], color=color, markersize=6, linestyle='none')
             cpt += 1
 
 plt.figure()
 ax = plt.gca()
-plt.xlim(0, xmax)
-plt.ylim(0, ymax)
-ax.set_xticks(np.arange(xmax + 1), minor=False)
-ax.set_yticks(np.arange(ymax + 1), minor=False)
-plt.grid(True, linewidth=2, color='gray')
-[plt.plot([x + 0.5], [y + 1], marker='o', color='k', markersize=6) for x in range(xmax) for y in range(ymax - 1)]
+plot_grid_layout()
+[plt.plot(x, [y - 0.5], marker='o', color='k', markersize=4) for x in range(xmax) for y in range(ymax + 1)]
 plot_points(5.7, 3.1, 'r')
 plot_points(2.7, 1.7, 'b')
 plot_points(1.2, 1.1, 'g')
+plot_points(1.2, 0.1, 'c')
 plt.title('Interpolation of V variables')
 savefig('interpolation_v_roms')
 # -
@@ -203,38 +235,36 @@ plt.rcParams['lines.markersize'] = 2
 
 plt.figure()
 ax = plt.gca()
-plt.xlim(0, xmax)
-plt.ylim(0, ymax)
-ax.set_xticks(np.arange(xmax + 1), minor=False)
-ax.set_yticks(np.arange(ymax + 1), minor=False)
-plt.grid(True, linewidth=2, color='gray')
+
+plot_grid_layout()
 
 color = 'firebrick'
-iout = [0, xmax, xmax, 0]
-jout = [0, 0, 1, 1]
+iout = [0 - 0.5, xmax + 0.5, xmax + 0.5, 0 - 0.5]
+jout = [0 - 0.5, 0 - 0.5, 0, 0]
 points = [(iii, jjj) for iii, jjj in zip(iout, jout)]
 p = ax.add_patch(Polygon(points, closed=True, facecolor=color, edgecolor=color, alpha = 0.1))
 
-color = 'firebrick'
-iout = [0, xmax, xmax, 0]
+color = 'blue'
+iout = [0 - 0.5, xmax + 0.5, xmax + 0.5, 0 - 0.5]
 jout = [ymax - 1, ymax - 1, ymax, ymax]
 points = [(iii, jjj) for iii, jjj in zip(iout, jout)]
 p = ax.add_patch(Polygon(points, closed=True, facecolor=color, edgecolor=color, alpha = 0.1))
 
-color = 'firebrick'
-jout = [1, 1, ymax - 1, ymax - 1]
+color = 'green'
+jout = [0, 0, ymax - 1, ymax - 1]
 iout = [xmax - 1, xmax, xmax, xmax - 1]
 points = [(iii, jjj) for iii, jjj in zip(iout, jout)]
 p = ax.add_patch(Polygon(points, closed=True, facecolor=color, edgecolor=color, alpha = 0.1))
 
-color = 'firebrick'
-jout = [1, 1, ymax - 1, ymax - 1]
-iout = [0, 1, 1, 0]
+color = 'orange'
+jout = [0, 0, ymax - 1, ymax - 1]
+iout = [-0.5, 0, 0, -0.5]
 points = [(iii, jjj) for iii, jjj in zip(iout, jout)]
 p = ax.add_patch(Polygon(points, closed=True, facecolor=color, edgecolor=color, alpha = 0.1))
 
 plt.title('Is On Edge')
 savefig('is_on_edge_roms', bbox='tight')
+
 # -
 
 # ### Land-sea mask
@@ -246,28 +276,25 @@ def plot_points(x, y, color):
 
     print('+++++++++++++ ', x, y)
     plt.plot(x, y, color=color, marker='o', markersize=6)
-    i = np.round(x - 0.5)
-    j = np.round(y - 0.5)
-    print(i, j)
+    i = np.round(x)
+    j = np.round(y)
+    print(f"mask([{j}, {i}]")
 
-    i -= 0.5
-    j -= 0.5
+    # i -= 0.5
+    # j -= 0.5
 
-    jout = [j + 0.5, j + 0.5, j + 1.5, j + 1.5]
-    iout = [i + 0.5, i + 1.5, i + 1.5, i + 0.5]
+    jout = [j - 0.5, j - 0.5, j + 0.5, j + 0.5]
+    iout = [i - 0.5, i + 0.5, i + 0.5, i - 0.5]
     points = [(iii, jjj) for iii, jjj in zip(iout, jout)]
     p = ax.add_patch(Polygon(points, closed=True, facecolor=color, edgecolor=color, alpha = 0.1))
 
 plt.figure()
 ax = plt.gca()
-plt.xlim(0, xmax)
-plt.ylim(0, ymax)
-ax.set_xticks(np.arange(xmax + 1), minor=False)
-ax.set_yticks(np.arange(ymax + 1), minor=False)
-plt.grid(True, linewidth=2, color='gray')
+plot_grid_layout()
 plot_points(5.7, 3.1, 'r')
 plot_points(2.7, 1.7, 'b')
-plot_points(1.2, 4.1, 'g')
+plot_points(1.2, 3.1, 'g')
+plot_points(0, 0, 'c')
 plt.title('Land-sea mask')
 savefig('land_sea_mask_roms', bbox='tight')
 
